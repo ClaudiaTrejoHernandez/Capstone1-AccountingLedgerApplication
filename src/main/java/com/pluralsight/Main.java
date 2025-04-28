78c5f8872f5ff021e500d1011e3e8c32818211da
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-04-28 13:55:55 -0700

    Created initial menu structure
    Added a basic structure of the menu the user will see.
    -I will then use this to guide me towards what classes I want to make

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,45 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static Scanner read = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        homeScreen();
+
     }
+
+    //Create a Home Screen:
+    public static void homeScreen(){
+        while (true){
+            System.out.println("Welcome to the Home Screen:");
+            System.out.println("D) Add Deposit ");
+            System.out.println("P) Make Payment (Debit) ");
+            System.out.println("L) Ledger ");
+            System.out.println("X) Exit ");
+            System.out.println("\nPlease type the letter of your command: ");
+            String userCommand = read.nextLine().trim().toUpperCase();
+
+            switch (userCommand) {
+                case "D":
+                    addDeposit();
+                    break;
+                case "P":
+                    makePayment();
+                    break;
+                case "L":
+                    ledgerMenu();
+                    break;
+                case "X":
+                    System.out.println("Exiting program. See you soon!");
+                    return;
+                default:
+                    System.out.println("Invalid command. Please try again");
+
+            }
+        }
+    }
+
 }
