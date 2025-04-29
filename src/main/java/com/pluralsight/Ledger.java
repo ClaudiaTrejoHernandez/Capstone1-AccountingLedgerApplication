14b939405515869bf49276c9772e63b495499d0c
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-04-29 10:00:51 -0700

    Testing private list
    -an error pop-up suggested to change my private list to final; I did but decided to keep it as is for now

--- src/main/java/com/pluralsight/Ledger.java
+++ src/main/java/com/pluralsight/Ledger.java
@@ -1,9 +1,57 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class Ledger {
 
-    //Class to add to ledger
-    private List<TransactionHelper>
+    private List<TransactionHelper> transactions;
+
+    //Method to create an array list
+    public Ledger(){
+        transactions = new ArrayList<>();
+    }
+
+    //Method to add transactions to Array List:
+    public void addTransaction(String description, String vendor, double amount){
+        LocalDateTime dateTime = LocalDateTime.now();
+        TransactionHelper userTransactions = new TransactionHelper(dateTime, description, vendor, amount);
+        transactions.add(userTransactions);
+
+        String transactionString = userTransactions.transactionString();
+        String filePath = "transactions.csv";
+
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath, true));
+            File file = new File(filePath);
+
+            if (!file.exists() || file.length() == 0) {
+                bufferedWriter.write("date|time|description|vendor|amount");
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.write(transactionString);
+            bufferedWriter.newLine();
+
+
+            if (amount > 0) {
+                System.out.println("Deposit of $" + amount + " was successfully processed.\n");
+            }
+
+            bufferedWriter.close();
+
+        } catch (IOException e) {
+            System.out.println("Unexpected error occurred: " + e.getMessage() + "\n");
+        }
+    }
+
+    //Method to view ledger info:
+    public List<TransactionHelper> getTransactions() {
+        return transactions;
+    }
 }
