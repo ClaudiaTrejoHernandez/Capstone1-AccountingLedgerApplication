0270b608036d672913ec4d08e6bb6e5dbaed82f4
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-04-29 09:59:25 -0700

    Adding more methods
    -Added ledger method to add transactions into the transactions.csv file
    -added deposit method
    -added make payment method
    -added comments to explain what the code is doing

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static Scanner read = new Scanner(System.in);
+    public static Ledger ledger = new Ledger();
 
     public static void main(String[] args) {
         homeScreen();
@@ -13,7 +14,7 @@ public class Main {
 
     //Create a Home Screen:
     public static void homeScreen() {
-        while (true) {
+        while (true) {                                                                  //Will continue to run until the user chooses to exit
             System.out.println("Welcome to the Home Screen:");
             System.out.println("D) Add Deposit ");
             System.out.println("P) Make Payment (Debit) ");
@@ -29,33 +30,57 @@ public class Main {
                 case "P":
                     makePayment();
                     break;
-                case "L":
-                    ledgerMenu();
-                    break;
-                case "X":
-                    System.out.println("Exiting program. See you soon!");
-                    return;
-                default:
-                    System.out.println("Invalid command. Please try again");
+//                case "L":
+//                    ledgerMenu();
+//                    break;
+//                case "X":
+//                    System.out.println("Exiting program. See you soon!");
+//                    return;
+//                default:
+//                    System.out.println("Invalid command. Please try again");
 
             }
         }
     }
        public static void addDeposit(){
-            System.out.println("Enter Deposit Amount: ");
-            double depositAmount = read.nextDouble();
-            if (depositAmount < 0){
-                System.out.println("Invalid input. Deposit amount must be positive.");
-            }
-
-            read.nextLine();
 
            System.out.println("Enter description of deposit: ");
            String depositDescription = read.nextLine();
            System.out.println("Enter vendor: ");
            String depositVendor = read.nextLine();
-        }
+           System.out.println("Enter Deposit Amount: ");
+           double depositAmount = read.nextDouble();
+           read.nextLine();
 
+           if (depositAmount <= 0) {
+               System.out.println("Invalid deposit. Deposit amount must be positive.");
+               return;
+           }
+
+           ledger.addTransaction(depositDescription, depositVendor, depositAmount);
+
+       }
+
+       public static void makePayment() {
+
+            System.out.println("Enter description of payment: ");
+            String paymentDescription = read.nextLine();
+            System.out.println("Enter vendor: ");
+            String paymentVendor = read.nextLine();
+            System.out.println("Enter payment amount");
+            double paymentAmount = read.nextDouble();
+            read.nextLine();
+
+            if (paymentAmount >= 0) {
+                System.out.println("Invalid payment. Payment amount must be negative.");
+                return;
+            }
+
+            ledger.addTransaction(paymentDescription, paymentVendor, paymentAmount);
+
+       }
+
+        
 
 
 
