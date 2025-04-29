f602be0f7933054b03ddffe30aab815721ec2de1
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-04-29 15:34:35 -0700

    Refactoring methods
    -refactored the bufferedWriter to have it save in one method instead of in every individual method
    -moved if conditions to here instead of having messages separated in here and in Main.

--- src/main/java/com/pluralsight/Ledger.java
+++ src/main/java/com/pluralsight/Ledger.java
@@ -1,9 +1,6 @@
 package com.pluralsight;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +9,7 @@ import java.util.List;
 public class Ledger {
 
     private List<TransactionHelper> transactions;
+    private static String filePath = "transactions.csv";
 
     //Method to create an array list
     public Ledger(){
@@ -19,39 +17,71 @@ public class Ledger {
     }
 
     //Method to add transactions to Array List:
-    public void addTransaction(String description, String vendor, double amount){
-        LocalDateTime dateTime = LocalDateTime.now();
-        TransactionHelper userTransactions = new TransactionHelper(dateTime, description, vendor, amount);
-        transactions.add(userTransactions);
+    public void addDeposit(String description, String vendor, double amount){
 
-        String transactionString = userDeposit.transactionString();
-        String filePath = "transactions.csv";
+        if (amount <= 0) {
+            System.out.println("Invalid deposit. Deposit amount must be positive.");
+            return;
+        }
 
+            amount = -Math.abs(amount);
+            TransactionHelper deposit = new TransactionHelper(LocalDateTime.now(), description, vendor, amount);
+            saveTransaction(deposit);
+
+        System.out.println("Deposit of $" + Math.abs(amount) + " was successfully processed.");
+
+    }
+
+    public void addPayment(String description, String vendor, double amount) {
+        if (amount <= 0) {
+            System.out.println("Invalid payment. Payment amount must be positive.");
+            return;
+        }
+
+        amount = -Math.abs(amount);
+        TransactionHelper payment = new TransactionHelper(LocalDateTime.now(), description, vendor, amount);
+        saveTransaction(payment);
+
+        System.out.println("Payment of $" + Math.abs(amount) + " was successfully processed.\n");
+
+    }
+
+    public void displayDeposit() {
+        for (TransactionHelper t : transactions) {
+            if (t.getAmount() > 0);
+            System.out.println(t.transactionString());
+        }
+    }
+
+    public void displayPayment() {
+        for (TransactionHelper t : transactions) {
+            if (t.getAmount() < 0);
+            System.out.println(t.transactionString());
+        }
+    }
+
+    public void displayAll() {
+        for (TransactionHelper t : transactions) {
+            System.out.println(t.transactionString());
+        }
+    }
+
+    //Method to write user input into csv file
+    public void saveTransaction(TransactionHelper t) {
         try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath, true));
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath));
             File file = new File(filePath);
 
             if (!file.exists() || file.length() == 0) {
                 bufferedWriter.write("date|time|description|vendor|amount");
                 bufferedWriter.newLine();
             }
-            bufferedWriter.write(transactionString);
+
+            bufferedWriter.write(t.transactionString());
             bufferedWriter.newLine();
 
-
-            if (amount > 0) {
-                System.out.println("Deposit of $" + amount + " was successfully processed.\n");
-            }
-
-            bufferedWriter.close();
-
         } catch (IOException e) {
-            System.out.println("Unexpected error occurred: " + e.getMessage() + "\n");
+            System.out.println("Unexpected error: " + e.getMessage());
         }
-    }
-
-    //Method to view ledger info:
-    public List<TransactionHelper> getTransactions() {
-        return transactions;
     }
 }
