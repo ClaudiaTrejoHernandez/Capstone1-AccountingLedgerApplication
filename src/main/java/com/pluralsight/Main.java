a15950ca53154cfcdfb1e02895a78467727b76e8
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-04-29 15:37:33 -0700

    Refactored and added methods in main
    -Made changes to syntax
    -Moved if statements to Ledger class to have everything in one place
    -Began working on ledgerMenu to have basic outline to continue building onto the Ledger class

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -15,7 +16,7 @@ public class Main {
     //Create a Home Screen:
     public static void homeScreen() {
         while (true) {                                                                  //Will continue to run until the user chooses to exit
-            System.out.println("Welcome to the Home Screen:");
+            System.out.println("\nWelcome to the Home Screen: \n");
             System.out.println("D) Add Deposit ");
             System.out.println("P) Make Payment (Debit) ");
             System.out.println("L) Ledger ");
@@ -30,14 +31,14 @@ public class Main {
                 case "P":
                     makePayment();
                     break;
-//                case "L":
-//                    ledgerMenu();
-//                    break;
-//                case "X":
-//                    System.out.println("Exiting program. See you soon!");
-//                    return;
-//                default:
-//                    System.out.println("Invalid command. Please try again");
+                case "L":
+                    ledgerMenu();
+                    break;
+                case "X":
+                    System.out.println("Exiting program. See you soon!");
+                    return;
+                default:
+                    System.out.println("Invalid command. Please try again");
 
             }
         }
@@ -52,12 +53,7 @@ public class Main {
            double depositAmount = read.nextDouble();
            read.nextLine();
 
-           if (depositAmount <= 0) {
-               System.out.println("Invalid deposit. Deposit amount must be positive.");
-               return;
-           }
-
-           ledger.addTransaction(depositDescription, depositVendor, depositAmount);
+           ledger.addDeposit(depositDescription, depositVendor, depositAmount);
 
        }
 
@@ -71,18 +67,76 @@ public class Main {
             double paymentAmount = read.nextDouble();
             read.nextLine();
 
-            if (paymentAmount >= 0) {
-                System.out.println("Invalid payment. Payment amount must be negative.");
-                return;
-            }
-
-            ledger.addTransaction(paymentDescription, paymentVendor, paymentAmount);
+            ledger.addPayment(paymentDescription, paymentVendor, paymentAmount);
 
        }
 
-        
+       public static void ledgerMenu() {
+           while (true) {
+               System.out.println("\nLedger Menu\n");
+               System.out.println("A) Display All");
+               System.out.println("D) Deposits");
+               System.out.println("P) Payments");
+               System.out.println("R) Reports");
+               System.out.println("\nPlease type the letter of your command: ");
+               String userCommand = read.nextLine().trim().toUpperCase();
 
+               switch (userCommand) {
+                   case "A":
+                       ledger.displayAll();
+                       break;
+                   case "D":
+                       ledger.displayDeposit();
+                       break;
+                   case "P":
+                       ledger.displayPayment();
+                       break;
+//                   case "R":
+//                       ledger.reports();
+//                        while (true) {
+//                            System.out.println("\nReports\n");
+//                            System.out.println("1) Month To Date");
+//                            System.out.println("2) Previous Month");
+//                            System.out.println("3) Year To Date");
+//                            System.out.println("4) Previous Year");
+//                            System.out.println("5) Search by Vendor");
+//                            System.out.println("0) Back");
+//                            System.out.println("\nPlease type the number of your command: ");
+//                            int ledgerCommand = read.nextInt();
+//                            read.nextLine();
+//
+//                            switch (ledgerCommand) {
+//                                case 1:
+//                                    ledger.monthToDate();
+//                                    break;
+//                                case 2:
+//                                    ledger.previousMonth();
+//                                    break;
+//                                case 3:
+//                                    ledger.yearToDate();
+//                                    break;
+//                                case 4:
+//                                    ledger.previousYear();
+//                                    break;
+//                                case 5:
+//                                    ledger.searchByVendor();
+//                                    break;
+//                                case 0:
+//                                    System.out.println("Returning to Ledger Menu");
+//                                    return;
+//                                default:
+//                                    System.out.println("Invalid command. Please try again");
+//                            }
+//                        }
+                   case "H":
+                       System.out.println("Returning to Home Menu\n");
+                       return;
+                   default:
+                       System.out.println("Invalid command. Please try again");
 
+               }
 
+           }
+       }
 
 }
