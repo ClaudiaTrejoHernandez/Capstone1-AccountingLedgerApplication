c1c2381fa8b4a128088f6749c967cacf1435a630
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-01 11:49:36 -0700

    Moved menu within a menu
    -I had the reports menu within the ledger menu and i was having a hard time returning from the reports menu to the ledger menu
    -to make it easier to understand i took out the reports menu and made it its own method.
    -it reads a lot better now and I can return to the ledger menu now

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -44,101 +44,108 @@ public class Main {
             }
         }
     }
-       public static void addDeposit(){
 
-           System.out.println("Enter description of deposit: ");
-           String depositDescription = read.nextLine();
-           System.out.println("Enter vendor: ");
-           String depositVendor = read.nextLine();
-           System.out.println("Enter Deposit Amount: ");
-           double depositAmount = read.nextDouble();
-           read.nextLine();
+    public static void addDeposit() {
 
-           ledger.addDeposit(depositDescription, depositVendor, depositAmount);
+        System.out.println("Enter description of deposit: ");
+        String depositDescription = read.nextLine();
+        System.out.println("Enter vendor: ");
+        String depositVendor = read.nextLine();
+        System.out.println("Enter Deposit Amount: ");
+        double depositAmount = read.nextDouble();
+        read.nextLine();
 
-       }
+        ledger.addDeposit(depositDescription, depositVendor, depositAmount);
 
-       public static void makePayment() {
+    }
 
-            System.out.println("Enter description of payment: ");
-            String paymentDescription = read.nextLine();
-            System.out.println("Enter vendor: ");
-            String paymentVendor = read.nextLine();
-            System.out.println("Enter payment amount");
-            double paymentAmount = read.nextDouble();
+    public static void makePayment() {
+
+        System.out.println("Enter description of payment: ");
+        String paymentDescription = read.nextLine();
+        System.out.println("Enter vendor: ");
+        String paymentVendor = read.nextLine();
+        System.out.println("Enter payment amount");
+        double paymentAmount = read.nextDouble();
+        read.nextLine();
+
+        ledger.addPayment(paymentDescription, paymentVendor, paymentAmount);
+
+    }
+
+    public static void ledgerMenu() {
+        while (true) {
+            System.out.println("\nLedger Menu\n");
+            System.out.println("A) Display All");
+            System.out.println("D) Deposits");
+            System.out.println("P) Payments");
+            System.out.println("R) Reports");
+            System.out.println("H) Return to Home Screen");
+            System.out.println("\nPlease type the letter of your command: ");
+            String userCommand = read.nextLine().trim().toUpperCase();
+
+            switch (userCommand) {
+                case "A":
+                    ledger.displayAll();
+                    break;
+                case "D":
+                    ledger.displayDeposit();
+                    break;
+                case "P":
+                    ledger.displayPayment();
+                    break;
+                case "R":
+                    reportsMenu();
+                    break;
+                case "H":
+                    System.out.println("Returning to Home Menu\n");
+                    return;
+                default:
+                    System.out.println("Invalid command. Please try again");
+
+            }
+        }
+    }
+
+    public static void reportsMenu() {
+        boolean stayInReports = true;
+        while (stayInReports) {
+            System.out.println("\nReports\n");
+            System.out.println("1) Month To Date");
+            System.out.println("2) Previous Month");
+            System.out.println("3) Year To Date");
+            System.out.println("4) Previous Year");
+            System.out.println("5) Search by Vendor");
+            System.out.println("0) Back");
+            System.out.println("\nPlease type the number of your command: ");
+            int ledgerCommand = read.nextInt();
             read.nextLine();
 
-            ledger.addPayment(paymentDescription, paymentVendor, paymentAmount);
-
-       }
-
-       public static void ledgerMenu() {
-            while (true) {
-               System.out.println("\nLedger Menu\n");
-               System.out.println("A) Display All");
-               System.out.println("D) Deposits");
-               System.out.println("P) Payments");
-               System.out.println("R) Reports");
-               System.out.println("H) Return to Home Screen");
-               System.out.println("\nPlease type the letter of your command: ");
-               String userCommand = read.nextLine().trim().toUpperCase();
-
-                switch (userCommand) {
-                   case "A":
-                       ledger.displayAll();
-                       break;
-                   case "D":
-                       ledger.displayDeposit();
-                       break;
-                   case "P":
-                       ledger.displayPayment();
-                       break;
-                   case "R":
-                        while (true) {
-                            System.out.println("\nReports\n");
-                            System.out.println("1) Month To Date");
-                            System.out.println("2) Previous Month");
-                            System.out.println("3) Year To Date");
-                            System.out.println("4) Previous Year");
-                            System.out.println("5) Search by Vendor");
-                            System.out.println("0) Back");
-                            System.out.println("\nPlease type the number of your command: ");
-                            int ledgerCommand = read.nextInt();
-                            read.nextLine();
-
-                            switch (ledgerCommand) {
-                                case 1:
-                                    ledger.monthToDateReport();
-                                    break;
-                                case 2:
-                                    ledger.previousMonthReport();
-                                    break;
-                                case 3:
-                                    ledger.yearToDateReport();
-                                    break;
-                                case 4:
-                                    ledger.previousYearReport();
-                                    break;
-                                case 5:
-                                    ledger.searchByVendor();
-                                    break;
-                                case 0:
-                                    System.out.println("Returning to Ledger Menu");
-                                    return;
-                                default:
-                                    System.out.println("Invalid command. Please try again");
-                            }
-                        }
-
-                   case "H":
-                       System.out.println("Returning to Home Menu\n");
-                       return;
-                   default:
-                       System.out.println("Invalid command. Please try again");
-
-               }
-
-           }
-       }
-
+            switch (ledgerCommand) {
+                case 1:
+                    ledger.monthToDateReport();
+                    break;
+                case 2:
+                    ledger.previousMonthReport();
+                    break;
+                case 3:
+                    ledger.yearToDateReport();
+                    break;
+                case 4:
+                    ledger.previousYearReport();
+                    break;
+                case 5:
+                    ledger.searchByVendor();
+                    break;
+                case 0:
+                    System.out.println("Returning to Ledger Menu");
+                    return;
+                default:
+                    System.out.println("Invalid command. Please try again");
+            }
+        }
+    }
 }
+
+
+
